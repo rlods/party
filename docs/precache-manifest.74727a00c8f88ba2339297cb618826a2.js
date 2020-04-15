94e5df0c3e0d7794f924ed07c7fca509b6a03ff7
Romain Lods <romain.lods@gmail.com>
2020-04-16 01:55:10 +0200

    Prod

---
+++ docs/precache-manifest.74727a00c8f88ba2339297cb618826a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625f996ebf8c0f89254ffe89b394b369",
+    "url": "/index.html"
+  },
+  {
+    "revision": "69905600f8ede024ee0c",
+    "url": "/static/css/main.7cbd4ef8.chunk.css"
+  },
+  {
+    "revision": "eb8c591e998f0a54dca8",
+    "url": "/static/js/2.74fb4475.chunk.js"
+  },
+  {
+    "revision": "82125b139ad61ebaa1f790a4468dd0ff",
+    "url": "/static/js/2.74fb4475.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69905600f8ede024ee0c",
+    "url": "/static/js/main.18c3d5d1.chunk.js"
+  },
+  {
+    "revision": "47b15c117c3d8fe568f8",
+    "url": "/static/js/runtime-main.a79c889c.js"
+  }
+]);
